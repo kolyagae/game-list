d973f8be69700192500586f59f6b6fa0f9a097e4
Zhenya Kolyaga <e.kolyaga@harddays.dev>
2025-07-11 17:21:43 +0300

    build: update project configuration for enhanced development

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: './src/app/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
     clean: true,
   },
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
     extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
     static: './dist',
     port: 3000,
     open: true,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'https://belparyaj.com/pragmatic',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: { '^/api': '' },
+      },
+    ],
   },
   module: {
     rules: [
@@ -25,7 +37,17 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: { localIdentName: '[name]__[local]--[hash:base64:5]' } },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
       },
     ],
   },
