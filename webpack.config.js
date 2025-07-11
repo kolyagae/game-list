3125c3a8b8348ec73d53f9e8c7183536186bb7f2
Zhenya Kolyaga <e.kolyaga@harddays.dev>
2025-07-11 17:56:09 +0300

    build: add redirects support via copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app/index.tsx',
@@ -54,6 +55,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public/_redirects', to: '' }],
     }),
   ],
   mode: 'development',
